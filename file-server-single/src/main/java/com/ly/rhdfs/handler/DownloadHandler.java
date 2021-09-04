a12bb2223c24e79a6ec549f281f2be75f918131c
menglong <xianlong.meng@qq.com>
2021-09-04 15:48:36 +0800

    create test module 1.debug single file self upload finish 2.debug single file self download finish range,if-range...

--- file-server-single/src/main/java/com/ly/rhdfs/handler/DownloadHandler.java
+++ file-server-single/src/main/java/com/ly/rhdfs/handler/DownloadHandler.java
@@ -37,35 +37,37 @@ public class DownloadHandler {
     private StoreFile storeFile;
     private ETagComputer eTagComputer;
     private ServerConfig serverConfig;
+
     @Autowired
-    public void setServerConfig(ServerConfig serverConfig){
-        this.serverConfig=serverConfig;
+    public void setServerConfig(ServerConfig serverConfig) {
+        this.serverConfig = serverConfig;
     }
+
     @Autowired
     public void setETagComputer(ETagComputer eTagComputer) {
         this.eTagComputer = eTagComputer;
     }
 
     @Autowired
-    public void setStoreFile(StoreFile storeFile){
-        this.storeFile=storeFile;
+    public void setStoreFile(StoreFile storeFile) {
+        this.storeFile = storeFile;
     }
 
     public Mono<ServerResponse> downloadFile(ServerRequest request) {
         String path = request.pathVariable("path");
-        if(org.apache.commons.lang3.StringUtils.isEmpty(path))
+        if (org.apache.commons.lang3.StringUtils.isEmpty(path))
             path = request.queryParam(serverConfig.getPathParamName()).orElse(ParamConstants.PARAM_PATH_NAME);
-        int index=Math.max(path.lastIndexOf("/"),path.lastIndexOf("\\"));
-        String fName=null;
-        if (index==-1) {
-            fName=path;
-            path="";
-        }else if (index==path.length()-1){
-            path=path.substring(0,index);
+        int index = Math.max(path.lastIndexOf("/"), path.lastIndexOf("\\"));
+        String fName = null;
+        if (index == -1) {
+            fName = path;
+            path = "";
+        } else if (index == path.length() - 1) {
+            path = path.substring(0, index);
             request.queryParam(serverConfig.getFileNameParamName()).orElse(ParamConstants.PARAM_FILE_NAME);
-        }else{
-            fName=path.substring(index+1);
-            path=path.substring(0,index);
+        } else {
+            fName = path.substring(index + 1);
+            path = path.substring(0, index);
         }
         // 1/If-Match(416) && If-Unmodified-Since(412)
         // 2/If-Modified-Since(304)
@@ -76,87 +78,51 @@ public class DownloadHandler {
         String filePath = path;
         String fileName = fName;
         if (StringUtils.isEmpty(fileName)) {
-            //文件资源不存在
+            // 文件资源不存在
             return ServerResponse.notFound().build();
         }
         String ifRange = request.headers().firstHeader(HttpHeaders.IF_RANGE);
-        List<HttpRange> rangeList=request.headers().range();
+        List<HttpRange> rangeList = request.headers().range();
         AtomicBoolean ifRangeResult = new AtomicBoolean(true);
         boolean etagFlag = false;
         int ifRangeType = 0;
         if (rangeList.isEmpty()) {
             ifRangeResult.set(false);
-        }else if (!StringUtils.isEmpty(ifRange)) {
+        } else if (!StringUtils.isEmpty(ifRange)) {
             if (ifRange.contains("\"")) {
                 if (ifRange.startsWith("W/")) {
-                    //弱比较
+                    // 弱比较
                     ifRangeType = 2;
                 } else {
                     ifRangeType = 1;
                 }
-                //需要比较etag
+                // 需要比较etag
                 etagFlag = true;
-            }else{
-                //比较时间
-                ifRangeType=3;
+            } else {
+                // 比较时间
+                ifRangeType = 3;
             }
         }
-        //构建返回下载文件资源的Mono
+        // 构建返回下载文件资源的Mono
         Mono<ServerResponse> responseMono = Mono.defer(() -> Mono.just(ifRangeResult)).flatMap(ifRangeResultValue -> {
             if (ifRangeResultValue.get()) {
-                //ifRange结果为真，返回部分range文件
-                return storeFile.loadFile(fileName, filePath, request.headers().range()).flatMap(fileRanges -> {
-                    if (fileRanges.getPartRangeList() == null && fileRanges.getResource() == null) {
-                        //未找到资源
-                        return ServerResponse.notFound().build();
-                    } else if (fileRanges.getResource() != null) {
-                        //直接返回资源，不分组
-                        return ServerResponse.ok().body((p, c) -> {
-                            ResourceHttpMessageWriter resp = new ResourceHttpMessageWriter();
-                            return resp.write(Flux.just(fileRanges.getResource()), null,
-                                    ResolvableType.forInstance(fileRanges.getResource()), MediaType.MULTIPART_FORM_DATA,
-                                    request.exchange().getRequest(), p, Hints.none());
+                // ifRange结果为真，返回部分range文件
+                return storeFile.loadFile(fileName, filePath, request.headers().range())
+                        .flatMap(fileRanges -> {
+                            if (fileRanges.getResource() != null) {
+                                // 直接返回资源，不分组
+                               return ServerResponse.ok().body((p, c) -> {
+                                        ResourceHttpMessageWriter resp = new ResourceHttpMessageWriter();
+                                        return resp.write(Flux.just(fileRanges.getResource()), null,
+                                                ResolvableType.forInstance(fileRanges.getResource()), MediaType.MULTIPART_FORM_DATA,
+                                                request.exchange().getRequest(), p, Hints.none());
+                                    });
+                            }else{
+                                return ServerResponse.notFound().build();
+                            }
                         });
-                    } else if (fileRanges.getPartRangeList().size() == 1) {
-                        //只有一个part，拼接后，直接返回
-                        PartRange partRange = fileRanges.getPartRangeList().get(0);
-                        return ServerResponse.status(HttpStatus.PARTIAL_CONTENT)
-                                .header(HttpHeaders.CONTENT_RANGE, fileRanges.toContentRange(partRange))
-                                .body((p, a) -> p.writeAndFlushWith(Flux.fromIterable(partRange.getResourceRegionList())
-                                        .flatMap(resourceRegion -> Flux
-                                                .just(FileChunkReader
-                                                        .readFile2Buffer(resourceRegion.getResource(),
-                                                                resourceRegion.getPosition(), resourceRegion.getCount())))));
-                    } else {
-                        //多个part，需要分别获取，并采用MultiPart的方法返回
-                        return ServerResponse
-                                .status(HttpStatus.PARTIAL_CONTENT)
-                                .body((p, c) -> {
-                                    MultipartHttpMessageWriter multiWriter = new MultipartHttpMessageWriter(
-                                            Arrays.asList(new EncoderHttpMessageWriter<>(new DataBufferEncoder()),
-                                                    new ResourceHttpMessageWriter()));
-                                    return multiWriter
-                                            .write(Flux.fromIterable(fileRanges.getPartRangeList())
-                                                            .collect(MultipartBodyBuilder::new,
-                                                                    (bodyBuild, partRange) -> bodyBuild
-                                                                            .asyncPart(partRange.getName(),
-                                                                                    Flux.fromIterable(partRange.getResourceRegionList())
-                                                                                            .flatMap(resourceRegion -> FileChunkReader
-                                                                                                    .readFile2Buffer(resourceRegion.getResource(),
-                                                                                                            resourceRegion.getPosition(),
-                                                                                                            resourceRegion.getCount())),
-                                                                                    DataBuffer.class))
-                                                            .map(MultipartBodyBuilder::build),
-                                                    ResolvableType.forClass(DataBuffer.class),
-                                                    MediaType.MULTIPART_FORM_DATA,
-                                                    p,
-                                                    Hints.none());
-                                });
-
-                    }
-                });
             } else {
-                //直接返回整个文件，使用zeroCopy
+                // 直接返回整个文件，使用zeroCopy
                 return ServerResponse.ok().body((p, a) -> {
                     var resp = (ZeroCopyHttpOutputMessage) p;
                     return resp.writeWith(storeFile.takeFilePath(fileName, filePath), 0,
@@ -164,33 +130,33 @@ public class DownloadHandler {
                 });
             }
         });
-        //验证
+        // 验证
         List<String> ifMatchList = request.headers().asHttpHeaders().getIfMatch();
         List<String> ifNoneMatchList = request.headers().asHttpHeaders().getIfNoneMatch();
         if (!ifMatchList.isEmpty() || !ifNoneMatchList.isEmpty()) {
-            //需要比较etag
+            // 需要比较etag
             etagFlag = true;
         }
         if (etagFlag) {
             int finalIfRangeType = ifRangeType;
-            //验证etag模式
+            // 验证etag模式
             return eTagComputer.etagFile(storeFile.takeFilePath(fileName, filePath)).flatMap(etag -> {
                 if (finalIfRangeType == 2) {
-                    //ifRange为etag的弱比较模式
+                    // ifRange为etag的弱比较模式
                     ifRangeResult.set(etag.contains(ifRange));
                 } else if (finalIfRangeType == 1) {
-                    //ifRange为etag的比较模式
+                    // ifRange为etag的比较模式
                     ifRangeResult.set(etag.equals(ifRange));
-                } else if (finalIfRangeType == 3){
-                    //时间比较模式
+                } else if (finalIfRangeType == 3) {
+                    // 时间比较模式
                     ifRangeResult.set(storeFile.takeFileUpdateTime(fileName, filePath)
                             .equals(DateFormatUtils.parseLocalDateTime4GMT(ifRange)));
                 }
-                //验证checkModify
+                // 验证checkModify
                 return request.checkNotModified(storeFile.takeFileInstant(fileName, filePath), etag);
             }).switchIfEmpty(responseMono);
         } else {
-            //验证ifRange的时间模式
+            // 验证ifRange的时间模式
             if (ifRangeType == 3) {
                 ifRangeResult.set(storeFile.takeFileUpdateTime(fileName, filePath)
                         .equals(DateFormatUtils.parseLocalDateTime4GMT(ifRange)));
