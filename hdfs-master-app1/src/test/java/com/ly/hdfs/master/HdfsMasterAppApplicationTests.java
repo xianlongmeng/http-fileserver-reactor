28e09fdebfe731f8b85605ffb84c6eb4e902f0b1
long <xianlong.meng@qq.com>
2023-07-22 15:14:02 +0800

    增加调试APP 连接管理问题调整 修改tcp的连接响应方式。 file chunk struct changed store interface update
    finish slice debug one step...
    SubscribeOn(Schedules.boundedElastic)
    update name
    create test module
    1.debug single file self upload finish
    2.debug single file self download finish range,if-range
    continue:
    1.single slice upload all finish,
    2.dft command truansfer
    3.master elect
    4.upload
    5.download

---
+++ hdfs-master-app1/src/test/java/com/ly/hdfs/master/HdfsMasterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.hdfs.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfsMasterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ hdfs-store-app2/src/main/java/com/ly/hdfs/store/HdfsStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.ly.hdfs.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfsStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdfsStoreAppApplication.class, args);
+    }
+
+}
