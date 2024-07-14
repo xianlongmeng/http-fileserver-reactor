72a1c4528be808f5c94fdf0105505ff52677810f
long <xianlong.meng@qq.com>
2024-07-14 13:29:49 +0800

    更新应用scanComponent范围 修改tcp的连接响应方式。 file chunk struct changed store interface update
    finish slice debug one step...
    SubscribeOn(Schedules.boundedElastic)
    update name
    create test module
    1.debug single file self upload finish
    2.debug single file self download finish range,if-range
    continue:
    1.single slice upload all finish,
    2.dft command truansfer
    3.master elect
    4.upload
    5.download

--- hdfs-store-app1/src/main/java/com/ly/hdfs/store/HdfsStoreAppApplication.java
+++ hdfs-store-app1/src/main/java/com/ly/hdfs/store/HdfsStoreAppApplication.java
@@ -2,8 +2,10 @@ package com.ly.hdfs.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.ly.common","com.ly.rhdfs"})
 public class HdfsStoreAppApplication {
 
     public static void main(String[] args) {
